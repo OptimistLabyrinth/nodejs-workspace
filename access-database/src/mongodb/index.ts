f7ec34c0f8dc884a71f1f87c29909fab80e07166
OptimistLabyrinth <keesung0705@gmail.com>
2022-10-31 12:21:22 +0900

    feat: install mongoose package

--- access-database/src/mongodb/index.ts
+++ access-database/src/mongodb/index.ts
@@ -1,6 +1,12 @@
+import mongoose from 'mongoose'
+
 function mongodbMain() {
   // eslint-disable-next-line no-console
   console.log('access mongodb')
+  mongoose.connect('mongodb://127.0.0.1:27017/test', () => {
+    // eslint-disable-next-line no-console
+    console.log('mongodb connected')
+  })
 }
 
 export { mongodbMain }

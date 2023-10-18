cf97c528438fc1ffbebbbebfb9edeeb225241b54
OptimistLabyrinth <keesung0705@gmail.com>
2023-10-18 13:48:13 +0900

    jest memory leaks occur no matter using either swc-jest or ts-jest

--- detect-memory-leaks-in-jest/jest.config.ts
+++ detect-memory-leaks-in-jest/jest.config.ts
@@ -1,5 +1,6 @@
 module.exports = {
   testMatch: ['**/test/**/test_*.+(ts|tsx)'],
   transform: { '^.+\\.(ts|tsx)$': '@swc/jest' },
+  // transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
   // setupFiles: ['jest-plugin-context/setup'],
 }
